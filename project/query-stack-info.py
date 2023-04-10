9f583761038593f7b04cb558a18c0cbccbe17db9
Xhy <103973379+FTS371@users.noreply.github.com>
2023-04-10 12:02:56 +0800

    更新 query-stack-info.py

--- project/query-stack-info.py
+++ project/query-stack-info.py
@@ -5,7 +5,7 @@
 # print(df) //Tushare
 import requests
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=002236.SHZ&apikey=EUV62MW1XVFCIS7T'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=002236.SHZ&apikey='
 r = requests.get(url)
 data = r.json()
 
